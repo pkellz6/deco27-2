5324adc8f70cb906ad38625948d9e53ced2af817
chacharoti <chacharoti@gmail.com>
2016-05-06 04:56:25 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505170924) do
+ActiveRecord::Schema.define(version: 20160506063526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,17 +68,20 @@ ActiveRecord::Schema.define(version: 20160505170924) do
     t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "photos", ["product_id"], name: "index_photos_on_product_id", using: :btree
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "description"
     t.integer  "category_id"
     t.string   "slug"
+    t.boolean  "is_feature",  default: false
+    t.string   "brand"
   end
 
   add_index "products", ["slug"], name: "index_products_on_slug", unique: true, using: :btree

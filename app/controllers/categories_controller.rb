1c651374f48233a2f4f2cfa9b1ca533b537dd987
chacharoti <chacharoti@gmail.com>
2016-05-06 04:54:49 -0700

    Do not authen when view categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,4 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_admin!, only: [ :show, :new, :create, :edit, :update, :destroy ]
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   # GET /categories

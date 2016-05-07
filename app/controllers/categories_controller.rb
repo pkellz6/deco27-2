a12cea08c858017a4cd13127f998a4dd8162b3df
chacharoti <chacharoti@gmail.com>
2016-05-07 06:14:31 -0700

    Add Pageination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @products = @category.products.page params[:page]
   end
 
   # GET /categories/new

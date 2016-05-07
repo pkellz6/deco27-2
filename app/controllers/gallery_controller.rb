5ee6788200461b30e1da3a12f7a036bb851d468a
chacharoti <chacharoti@gmail.com>
2016-05-07 06:17:56 -0700

    Add Pageination for Gallery

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,6 +1,7 @@
 class GalleryController < ApplicationController
   def index
   	@gallery = Category.new(name: 'Gallery')
+  	@products = Product.where(is_feature: true).page params[:page]
     render :template => "categories/show"
   end
 end

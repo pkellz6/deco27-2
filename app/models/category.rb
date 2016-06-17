f87a84829d645ef8b8a5ae3713adda69cce4f2f0
Angel <a.angel.martinez@gmail.com>
2016-06-17 18:23:57 +0000

    add small edits from pat

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,12 +8,18 @@ class Category < ActiveRecord::Base
   VALIDATE_SIZE = { :in => 0..5.megabytes, :message => 'Photo size too large. Please limit to 5 mb.' }
   has_attached_file :picture,
                     :styles => { large: '876x239#', medium: '270x270#', thumb: '100x100#' },
-                    :default_url => DEFAULT_URL
+                    :default_url => DEFAULT_URL, :storage => :s3, :s3_credentials => Proc.new{|a| a.instance.s3_credentials}
   validates_attachment  :picture,
                         :content_type => { :content_type => /\Aimage\/.*\Z/},
                         :size => VALIDATE_SIZE
 
+
 	def should_generate_new_friendly_id?
     slug.blank?
   end
+
+	def s3_credentials
+    {:bucket => ENV['s3_bucket'], :access_key_id => ENV['aws_access_key_id'], :secret_access_key => ENV['aws_access_secret']}
+  end
+
 end
